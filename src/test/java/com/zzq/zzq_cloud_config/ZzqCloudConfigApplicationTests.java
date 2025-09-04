7efb718e02c53a329f63f5fbf0d8efa4b3903d69
Andrea <Andrea.Chen@verystar.cn>
2025-09-04 16:01:02 +0800

    Init

---
+++ src/test/java/com/zzq/zzq_cloud_config/ZzqCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzq.zzq_cloud_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzqCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
